ba3aa2e42bb1188d6202336c76a116c68c7315a1
Matthew Morgan <clinrsxbox@gmail.com>
2018-01-07 19:12:22 +0000

    Bot files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Verification
 The public discord bot that acts like a social media verification thing.
+
+# Contributing
+Anyone can contribute to this project is much appreciated to improve the bot.
+
+# Contributers
+None at the moment.
---
+++ index.js
@@ -0,0 +1,86 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = "" // Recommended to load from json file.
+const prefix = "."
+global.botuser = null
+client.on('ready', () => {
+  console.log('I am ready!');
+  botuser = client.user;
+  client.user.setGame('Verification at your service!');
+});
+
+client.on('message', message => {
+  var cmd = message.content.split(' ');
+  var user = message.member;
+  if (cmd[0] == prefix+'ping') {
+    message.delete();
+    message.reply('pong!');
+  }
+  if(cmd[0] == prefix+'help' && user.hasPermissions(8) == true) {
+    message.delete();
+    message.reply("Hi! I'm Verification, the public discord bot that acts like a social media verification thing.");
+    message.reply("Anyways, commands are:");
+    message.reply(prefix+"help - You're seeing this right now!");
+    message.reply(prefix+"ping - ping pong!");
+    // TODO : message.reply(prefix+"help-on-role - How to create \"Verified Accounts\" role.");
+    message.reply(prefix+"verifyuser <userid> - Add this user to role \"Verified Accounts\".");
+    message.reply(prefix+"remverified <userid> - Remove this user from role \"Verified Accounts\".");
+  }
+  if (cmd[0] == prefix+'verifyuser' && user.hasPermissions(8) == true) {
+    message.delete();
+    if(cmd[1] != null) {
+      var username = cmd[1].replace("@", "");
+      const user = message.guild.member(cmd[1])
+      if(!user){
+        message.reply("No username found!");
+      } else if(user) {
+        const r = message.guild.roles.find("name", "Verified Accounts");
+        if(!r) {
+          message.reply(
+            'Please create a new role named exactly "Verified Accounts".'
+          );
+          return;
+        }
+        const isinrole = user.roles.find("name", "Verified Accounts");
+        if(isinrole)
+        {
+          message.reply("This username is already Verified!");
+          return;
+        }
+        user.addRole(r);
+        //399437752275304468 checkmark
+
+        message.reply("User now verified! :white_check_mark:");
+      }
+    }
+}
+    if (cmd[0] == prefix+'remverified' && user.hasPermissions(8) == true) {
+      message.delete();
+      if(cmd[1] != null) {
+        var username = cmd[1].replace("@", "");
+        const user = message.guild.member(cmd[1])
+        if(!user){
+          message.reply("No username found!");
+        } else if(user) {
+          const r = message.guild.roles.find("name", "Verified Accounts");
+          if(!r) {
+            message.reply(
+              'Please create a new role named exactly "Verified Accounts".'
+            );
+            return;
+          }
+          const isinrole = user.roles.find("name", "Verified Accounts");
+          if(!isinrole)
+          {
+            message.reply("This username has not been Verified!");
+            return;
+          }
+          user.removeRole(r);
+          //399437752275304468 checkmark
+          message.reply("User unverified! *thumbs down*");
+        }
+      }
+  }
+});
+
+client.login(token);
